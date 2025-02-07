ed7396bb751955c62388f81a9f979ff03bedc9f0
Alejandro <ale12@mit.edu>
2025-02-07 09:16:34 -0500

    Fix deployment: Ensure frontend builds on Heroku

--- server/index.js
+++ server/index.js
@@ -44,10 +44,10 @@ app.use("/api", api);
 const port = process.env.PORT || 5000;
 
 // Serve production build
-app.use(express.static(path.join(__dirname, "../client/build/")))
+app.use(express.static(path.join(__dirname, "../client/dist/")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/public/dist/index.html"));
 });
 
 app.listen(port, () =>
