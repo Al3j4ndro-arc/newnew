dfad276f3ec3737d8f901bea2773cc7d88836c65
Alejandro <ale12@mit.edu>
2025-02-07 09:28:26 -0500

    Fix deployment: Ensure frontend correct on Heroku

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.use("/api", api);
 const port = process.env.PORT || 5000;
 
 // Serve production build
-app.use(express.static(path.join(__dirname, "../client/dist/")))
+app.use(express.static(path.join(__dirname, "../client/public/dist/")))
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/public/dist/index.html"));
