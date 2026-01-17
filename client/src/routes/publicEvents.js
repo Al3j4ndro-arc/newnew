954b0f3723ddf70ce3e561ae74f27970eb198aa1
Alejandro <ale12@mit.edu>
2026-01-17 16:47:01 +0100

    Fall 2025 -> Spring 2026

--- client/src/routes/publicEvents.js
+++ client/src/routes/publicEvents.js
@@ -58,7 +58,7 @@ export default function PublicEvents() {
             <div className="bg-gray-50 ">
                 <div className='text-center'>
                     <h1 className="pt-8 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                        Welcome to MCG's Fall 2025 Recruitment Cycle Events
+                        Welcome to MCG's Spring 2026 Recruitment Cycle Events
                     </h1>
                     <h3 className="my-4">
                         Please{" "}
